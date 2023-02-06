a9f1d142091d456f8b49fad9cbf326af0a416b6d
Rayhan <mazharrayhan3737@gmail.com>
2023-02-07 00:34:16 +0600

    new item is successfully added by the logged in user

--- item/urls.py
+++ item/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import detail, add_new_item
 
 app_name = 'item'
 
 urlpatterns = [
-    path('<int:pk>/', views.detail, name="detail"),
+    path('<int:pk>/', detail, name="detail"),
+    path('new/', add_new_item, name="newitem"),
 ]
--- item/views.py
+++ item/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,get_object_or_404
+from .form import NewItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -9,4 +11,20 @@ def detail(request, pk):
     return render(request, 'item/detail.html',{
         'item': item,
         'related_items':related_items,
+    })
+
+@login_required
+def add_new_item(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+        else:
+            form = NewItemForm()
+            
+    return render(request,'item/add_new_item.html',{
+        'form':form,
+        'title': 'New Item',
     })
