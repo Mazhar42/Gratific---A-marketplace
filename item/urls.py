30faedc24cd447c63f19b995798749701c8e8d86
Rayhan <mazharrayhan3737@gmail.com>
2023-02-07 19:05:47 +0600

    Search functionality added

--- item/urls.py
+++ item/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import detail, add_new_item,delete_item,edit_item
+from .views import detail, add_new_item,delete_item,edit_item,search
 
 app_name = 'item'
 
 urlpatterns = [
+    path('', search, name="search"),
     path('<int:pk>/', detail, name="detail"),
     path('<int:pk>/delete/', delete_item, name="delete"),
     path('<int:pk>/edit/', edit_item, name="edit"),
